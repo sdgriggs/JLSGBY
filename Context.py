70b7fa8875fed0e8e6d293dcacbf837ac3e41143
sdgriggs <sdgriggs@ncsu.edu>
2023-04-08 14:59:22 -0400

    Being Listing of Plants in UI

--- Context.py
+++ Context.py
@@ -18,5 +18,21 @@ class Context:
     white = (255, 255, 255)
     black = (0, 0, 0)
 
-    def _handle_click_event(self, event):
-        pass
+
+    # define click regions
+    click_regions = []
+
+    # define plants
+    plants = ["Plant 1", "A PLANT WITH A SUPER LONG NAME 2", "Plant 3"]
+
+
+    def _handle_click_event(self, event, mouse):
+        for region in self.click_regions:
+            if 
+        
+
+    def init_click_regions(self):
+        self.click_regions = []
+
+    def append_click_region(self, x, y, width, height, str):
+        self.click_regions.append({'x':x, 'y':y, 'width':width, 'height':height, 'desc': str})
--- runner.py
+++ runner.py
@@ -1,7 +1,6 @@
 import pygame
 import pygame.locals
 from Context import Context
-
 if __name__ == '__main__':
 
     pygame.init()
@@ -11,7 +10,10 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     running = True
     c = 0
+    smallfont = pygame.font.SysFont('Corbel',35)
+
     context = Context()
+
     while running:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
@@ -19,7 +21,7 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                context._handle_click_event(event)
+                context._handle_click_event(event, pygame.mouse)
 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill(pygame.Color(240,231,231))
@@ -35,6 +37,27 @@ if __name__ == '__main__':
         #Left information pannel
         left_pannel_width = infoObject.current_w /4 * 3
         pygame.draw.rect(screen, pygame.Color(193,168,14), pygame.Rect(0, 0, left_pannel_width, infoObject.current_h))
+        #Allocate the areas where clicks are valid
+        context.init_click_regions()
+        for i in range(0, len(context.plants)):
+            b = context.plants[i]
+            text = smallfont.render(b, True, 'black')
+            rect = text.get_rect()
+            rect.x = 100
+            rect.y = 100 * i + 200
+            pygame.draw.rect(screen, pygame.Color(193, 168, 14), rect)
+            screen.blit(text, rect)
+            rect.x = rect.topright[0]
+            rect.width = 50
+            pygame.draw.rect(screen, pygame.Color(0, 255, 0), rect, border_radius=25)
+            context.append_click_region(rect.x, rect.y, rect.width, rect.heigh, b  + "-buy")
+            rect.x = 50
+            rect.width = 50
+            pygame.draw.rect(screen, pygame.Color(255, 0, 0), rect, border_radius=25)
+            context.append_click_region(rect.x, rect.y, rect.width, rect.heigh, b  + "-sell")
+
+
+
 
         #TOP Status Bar
         pygame.draw.rect(screen, pygame.Color(69,24,4), pygame.Rect(0,0,infoObject.current_w, 100), border_bottom_left_radius=25, border_bottom_right_radius=25)
