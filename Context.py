b2fe9cc4c3fc995d5a609fb57d5aa459fada41de
sdgriggs <sdgriggs@ncsu.edu>
2023-04-09 04:29:02 -0400

    Update thresh

--- Context.py
+++ Context.py
@@ -153,9 +153,9 @@ class Context:
     #mapping for uv
     UV_MAPPING = {'Very_High': 4, 'High': 3, 'Moderate': 2, 'Low':1}
 
-    PEOPLE_THRESH = 53
+    PEOPLE_THRESH = 15000
 
-    ADTL_PERSON_THRESH = 10
+    ADTL_PERSON_THRESH = 500
 
     first_person = "Insufficent-Food"
     # Resource Counters
